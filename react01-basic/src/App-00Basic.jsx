2ed58f6c50947a4b1cbf2cd774e1d859210ae26d
tnfla1130 <ksr113030@gmail.com>
2025-05-13 17:53:10 +0900

    React

---
+++ react01-basic/src/App-00Basic.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (<>
+     <h2>처음 만든 React 프로젝트</h2>
+    </>)
+}
+
+export default App
---
+++ react01-basic/src/App-01Start.jsx
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <div className="App">
+      <h2>React - 기본</h2>
+      <ol>
+        <li>프론트앤드</li>
+        <ul>
+          <li>HTML5</li>
+          <li>CSS3</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
+        </ul>
+        <li>백앤드</li>
+        <ul>
+          <li>Java</li>
+          <li>Oracle</li>
+          <li>JSP</li>
+          <li>Sprint Boot</li>
+        </ul>
+      </ol>
+    </div>
+  );
+}
+
+export default App
